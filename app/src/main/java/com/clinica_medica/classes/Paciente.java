349fdc3d3e6e26c640147e70d2964bb057891edc
Raquel Macedo de Araújo <PET0047451@aluno.firjansenaisesi.com.br>
2023-08-23 11:58:28 -0300

    Montagem da classe Pacientes

---
+++ app/src/main/java/com/clinica_medica/classes/Paciente.java
@@ -0,0 +1,40 @@
+package com.clinica_medica.classes;
+
+public class Paciente {
+    String nome; 
+    String cpf;
+    String dataNascimento;
+    String endereco;
+    
+    public Paciente(String nome, String cpf, String dataNascimento, String endereco) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+        this.endereco = endereco;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCpf() {
+        return cpf;
+    }
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+    public String getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+    
+}
